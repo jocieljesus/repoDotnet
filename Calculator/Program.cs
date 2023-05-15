e9510bed81c536e1aaa3e912dee2b67e4b118497
jociel <jocielalvesdejesus@gmail.com>
2023-05-15 00:30:07 -0300

    app calculator v1

---
+++ Calculator/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string res;
+            do{
+                Menu();
+                Console.WriteLine("Quer voltar a calculadora? s/n");
+                res = Console.ReadLine();
+            } while ( res.ToLower() == "s");
+            
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("O que deseja fazer? ");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            Console.WriteLine("5 - Sair");
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Selecione uma opção: ");
+            short opcao = short.Parse(Console.ReadLine());
+            
+            Console.Clear();
+
+            switch(opcao)
+            {   
+                case 1:
+                    Soma();
+                    break;
+                case 2:
+                    Subtracao();
+                    break;
+                case 3:
+                    Multiplicacao();
+                    break;
+                case 4:
+                    Divisao();
+                    break;
+                case 5:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Opção Inválida!");
+                    Menu();
+                    break;
+            }
+        }
+        static void Soma()
+        {
+            Console.WriteLine("Informe um numero: ");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe outro numero: ");
+            double v2 = double.Parse(Console.ReadLine());
+
+            double soma = v1 + v2;
+            Console.WriteLine($"O resultado da soma é {soma}");
+        }
+
+        static void Subtracao()
+        {
+            Console.WriteLine("Informe um numero: ");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe outro numero: ");
+            double v2 = double.Parse(Console.ReadLine());
+
+            double subtracao = v1 - v2;
+            Console.WriteLine($"O resultado da subtração é {subtracao}");
+        }
+
+        static void  Multiplicacao()
+        {
+            Console.WriteLine("Informe um numero: ");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe outro numero: ");
+            double v2 = double.Parse(Console.ReadLine());
+
+            double multiplicacao = v1 * v2;
+            Console.WriteLine($"O resultado da multiplicação é {multiplicacao}");
+        }
+
+        static void Divisao()
+        {
+            Console.WriteLine("Informe um numero: ");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe outro numero: ");
+            double v2 = double.Parse(Console.ReadLine());
+
+            if(v2 == 0 ){
+                Console.WriteLine("ATENÇÃO: Não é possível realizar divisão por 0");
+            } else {
+                double divisao = v1 / v2;
+                Console.WriteLine($"O resultado da divisõa é {divisao}");
+            }
+            
+        }
+    }
+}
---
+++ MeuApp/Program.cs
@@ -0,0 +1,49 @@
+﻿
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+           Cliente cliente = new Cliente;
+
+           cliente.Nome = "Jociel";
+           
+        }
+
+    }
+
+    struct Product 
+    {
+        public Product(int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+        public int Id;
+        public string Name;
+        public double Price;
+        
+        public double PriceInDolar( double dolar)
+        {
+            return Price * dolar;
+        }
+    }
+
+    struct Cliente
+    {
+        public string Nome;
+        public EEstadoCivil estadoCivil;
+        
+    }
+
+    enum EEstadoCivil
+    {
+        BRL = 748,
+        EUR = 328,
+        USD  = 034,
+        GPB = 048
+
+    }
+} 
