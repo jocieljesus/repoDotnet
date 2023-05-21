bc07290a5af6d07c9fdaa546fb6e0629aa203bef
jociel <jocielalvesdejesus@gmail.com>
2023-05-21 00:49:02 -0300

    projeto editor html

---
+++ EditorHtml/Editor.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace EditorHtml
+{
+    public class Editor
+    {
+
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("MODO EDITOR");
+            Console.WriteLine("---------------");
+            Start();
+        }
+        public static void Start()
+        {
+            var file = new StringBuilder();
+
+            do
+            {
+                file.Append(Console.ReadLine());
+                file.AppendLine(Environment.NewLine);
+            }
+            while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.WriteLine("---------------");
+            Console.WriteLine("Deseja salvar o arquivo?");
+            Viewer.Show(file.ToString());
+
+        }
+    }
+}
